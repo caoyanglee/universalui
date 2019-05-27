c773c8808d93153978d4a19c5d21ea103a760881
你需要一台永动机 <779213209@qq.com>
2019-05-27 16:46:11 +0800

    增加判断颜色是否是浅色的的扩展方法

---
+++ universalview/src/main/java/com/weimu/universalview/ktx/Color.kt
@@ -0,0 +1,9 @@
+package com.weimu.universalview.ktx
+
+import android.graphics.Color
+
+//是否是亮色
+fun Int.isLightColor(): Boolean {
+    val darkness = 1 - (0.299 * Color.red(this) + 0.587 * Color.green(this) + 0.114 * Color.blue(this)) / 255
+    return darkness < 0.5
+}
