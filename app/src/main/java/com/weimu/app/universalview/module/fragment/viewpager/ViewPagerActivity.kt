e41b241a28cf67af7b7119da655370c600cfd0bb
你需要一台永动机 <779213209@qq.com>
2019-09-25 17:09:26 +0800

    增加TabView的Demo代码

--- app/src/main/java/com/weimu/app/universalview/module/fragment/viewpager/ViewPagerActivity.kt
+++ app/src/main/java/com/weimu/app/universalview/module/fragment/viewpager/ViewPagerActivity.kt
@@ -8,7 +8,10 @@ import com.orhanobut.logger.Logger
 import com.weimu.app.universalview.R
 import com.weimu.app.universalview.base.BaseViewActivity
 import com.pmm.ui.core.pager.BaseFragmentPagerAdapter
+import com.pmm.ui.interfaces.MyViewPagerChangeListener
 import com.pmm.ui.ktx.click
+import com.pmm.ui.ktx.toast
+import com.pmm.ui.widget.TabView
 import kotlinx.android.synthetic.main.activity_view_pager.*
 
 class ViewPagerActivity : BaseViewActivity() {
@@ -27,7 +30,6 @@ class ViewPagerActivity : BaseViewActivity() {
                     this.click { onBackPressed() }
                 }
 
-
         viewpager.adapter = BaseFragmentPagerAdapter(supportFragmentManager).apply {
             val fragments = arrayListOf<Fragment>()
             fragments.add(Section1Fragment.newInstance(this@ViewPagerActivity))
@@ -36,6 +38,24 @@ class ViewPagerActivity : BaseViewActivity() {
             setFragments(fragments)
         }
         viewpager.offscreenPageLimit = 3
+        viewpager.addOnPageChangeListener(object : MyViewPagerChangeListener() {
+            override fun onPageSelected(position: Int) {
+                mTabView.position = position
+            }
+
+        })
+
+        mTabView.setData(
+                TabView.TabData("1"),
+                TabView.TabData("2"),
+                TabView.TabData("3"))
+        mTabView.onTabClick = {
+            viewpager.currentItem = it
+        }
+        mTabView.onTabReClick = {
+            toast("点到第${it}Fragment")
+        }
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
