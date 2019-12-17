6ecbb4e4570d9c912f4839c7cb14750a0b79c133
你需要一台永动机 <779213209@qq.com>
2019-12-18 02:04:31 +0800

    整理扩展

--- lib/src/main/java/com/pmm/ui/ktx/RecyclerVIew.kt
+++ lib/src/main/java/com/pmm/ui/ktx/RecyclerVIew.kt
@@ -1,5 +1,9 @@
 package com.pmm.ui.ktx
 
+import android.animation.ObjectAnimator
+import android.os.Handler
+import android.view.View
+import android.view.animation.AccelerateInterpolator
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -23,4 +27,54 @@ fun RecyclerView.init(): RecyclerView {
     (this.itemAnimator as SimpleItemAnimator).changeDuration = 0
     this.layoutManager = LinearLayoutManagerPro(context, LinearLayoutManager.VERTICAL, false)
     return this
+}
+
+/**
+ * @param views 目标视图
+ * @param scrollHeight 滚动的距离 单位px
+ */
+fun RecyclerView.addScrollToBottomAnimation(vararg views: View, scrollHeight: Float = 0f, animationTime: Long = 200) {
+    var isViewGone = false
+    var isAnimationRun = false
+
+    fun scroll2Bottom() {
+        if (isAnimationRun || isViewGone) return
+        isAnimationRun = true
+        for (item in views) {
+            val animator = ObjectAnimator.ofFloat(item, "translationY", 0f, scrollHeight).setDuration(animationTime)
+            animator.interpolator = AccelerateInterpolator()
+            animator.start()//图片
+        }
+        Handler().postDelayed({
+            isAnimationRun = false
+            isViewGone = true
+        }, animationTime)
+    }
+
+    fun scroll2Top() {
+        if (isAnimationRun || !isViewGone) return
+        isAnimationRun = true
+        for (item in views) {
+            val animator = ObjectAnimator.ofFloat(item, "translationY", scrollHeight, 0f).setDuration(animationTime)
+            animator.interpolator = AccelerateInterpolator()
+            animator.start()//图片
+        }
+        Handler().postDelayed({
+            isAnimationRun = false
+            isViewGone = false
+        }, animationTime)
+    }
+
+    this.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+            if (Math.abs(dy) <= 20) return//偏移量必须大于20
+            if (dy > 0) {
+                scroll2Bottom()//上拉
+            } else {
+                scroll2Top() //下拉
+            }
+        }
+    })
 }
