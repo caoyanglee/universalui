668af8fb5131671504019fc510073f3062070a01
你需要一台永动机 <779213209@qq.com>
2020-03-09 14:37:09 +0800

    增加EventBus的粘性通知

--- lib/src/main/java/com/pmm/ui/helper/EventBusPro.kt
+++ lib/src/main/java/com/pmm/ui/helper/EventBusPro.kt
@@ -18,19 +18,22 @@ object EventBusPro {
 
     //注册
     fun register(subscriber: Any) {
-        if (!EventBus.getDefault().isRegistered(subscriber))
-            EventBus.getDefault().register(subscriber)
+        if (!EventBus.getDefault().isRegistered(subscriber)) EventBus.getDefault().register(subscriber)
     }
 
     //注销
     fun unregister(subscriber: Any) {
-        if (EventBus.getDefault().isRegistered(subscriber))
-            EventBus.getDefault().unregister(subscriber)
+        if (EventBus.getDefault().isRegistered(subscriber)) EventBus.getDefault().unregister(subscriber)
     }
 
     //发送通知
     fun post(obj: Any) {
         EventBus.getDefault().post(obj)
+    }
+
+    //发送粘性通知
+    fun postSticky(obj: Any) {
+        EventBus.getDefault().postSticky(obj)
     }
 
     //发送延迟通知
