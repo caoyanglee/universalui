ff5065641d7fa8122b18225be5216a0b88df3fc1
你需要一台永动机 <779213209@qq.com>
2020-05-06 22:32:46 +0800

    增加setStatusNavigationBarTransparent方法
    一：状态栏沉浸
    二：操作栏沉浸

--- lib/src/main/java/com/pmm/ui/core/toolbar/StatusBarManager.kt
+++ lib/src/main/java/com/pmm/ui/core/toolbar/StatusBarManager.kt
@@ -15,11 +15,10 @@ object StatusBarManager {
 
     private var statusLightBarColorV5 = Color.rgb(102, 102, 102)//5.0及其5.1亮色状态背景
 
-
     /**
      * 修改状态栏为全透明
      */
-    fun setTransparencyBar(window: Window?) {
+    fun setStatusBarTransparent(window: Window?) {
         window?.apply {
             this.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
             this.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -27,6 +26,23 @@ object StatusBarManager {
             this.statusBarColor = Color.TRANSPARENT
         }
     }
+
+    /**
+     * 修改状态栏 & 导航栏 透明
+     */
+    fun setStatusNavigationBarTransparent(window: Window?) {
+        window?.apply {
+            this.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+            this.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            this.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            this.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+            this.navigationBarColor = Color.TRANSPARENT
+            this.statusBarColor = Color.TRANSPARENT
+        }
+    }
+
 
     /**
      * 设置状态栏的背景颜色
@@ -73,7 +89,7 @@ object StatusBarManager {
             result = 1
         } else if (FlymeSetStatusBarLightMode(window, true, isFullScreen)) {
             result = 2
-        } else {//5.x版本的手机不支持状态栏你黑色字体，所以状态栏的颜色自己看设置一个
+        } else {//5.x版本的手机不支持状态栏里黑色字体，所以状态栏的颜色自己看设置一个
             setColor(window, statusLightBarColorV5)
         }
         return result
