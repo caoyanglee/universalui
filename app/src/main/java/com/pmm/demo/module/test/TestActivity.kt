af8bb798ad9cf3f6e37849d7c3b4614e8b155be0
你需要一台永动机 <779213209@qq.com>
2020-08-19 11:12:50 +0800

    请求权限的Demo

--- app/src/main/java/com/pmm/demo/module/test/TestActivity.kt
+++ app/src/main/java/com/pmm/demo/module/test/TestActivity.kt
@@ -1,10 +1,13 @@
 package com.pmm.demo.module.test
 
+import android.Manifest
 import android.app.Activity
 import android.os.Bundle
 import com.pmm.demo.R
 import com.pmm.ui.ktx.click
 import com.pmm.demo.base.BaseViewActivity
+import com.pmm.ui.ktx.requestPermission
+import com.pmm.ui.ktx.toast
 import kotlinx.android.synthetic.main.activity_test.*
 
 
@@ -26,5 +29,15 @@ class TestActivity : BaseViewActivity() {
             onBackPressed()
         }
 
+        mBtn5.click {
+            this.requestPermission(
+                    permissions = *arrayOf(
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                    ),
+                    granted = { toast("拿到权限了") },
+                    content = "您需要给小盛权限，才能正常访问存储文件哦"
+            )
+        }
     }
 }
